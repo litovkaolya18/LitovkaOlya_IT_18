54139edcfae8838c0dbc3ef9c7f858041e08db7f
litovkaolya18 <olakim710@gmail.com>
2025-01-10 21:49:38 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,5 +2,11 @@
 #include <string>
 using namespace std;
 
-//преобразования числа из строки в шестнадцатеричную строку
-void hex_16(const string& num_str, string& hex_str);
+//функция для перевода числа из 10 в 16 систему счисления
+string hex_16(const string& num_str);
+
+//функция для деления числа на 16
+void num_conversion(const string& num_str, int& ostat);
+
+//функция для проверки, корректен ли ввод 
+bool is_correct(const string& num_str);
